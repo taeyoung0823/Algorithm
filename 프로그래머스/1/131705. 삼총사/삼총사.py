18dd28fb21b1beb5cdcb796ec863f868cec33693
taeyoung0823 <154206361+taeyoung0823@users.noreply.github.com>
2024-11-07 12:24:48 +0900

    [level 1] Title: 삼총사, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,8 @@
+def solution(number):
+    result = 0
+    for i in range(0,len(number)):
+        for j in range(i+1,len(number)):
+            for k in range(j+1,len(number)):
+                if number[i]+number[j]+number[k]==0:
+                    result +=1
+    return result
