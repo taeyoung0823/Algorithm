6119cd3ca807181e4a40521aa96967a458d62a5a
taeyoung0823 <154206361+taeyoung0823@users.noreply.github.com>
2024-11-17 00:02:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,13 @@
+def solution(n, words):
+    used_words = set()
+    used_words.add(words[0])
+
+    for i in range(1, len(words)):
+        if words[i][0] != words[i-1][-1] or words[i] in used_words:
+            person = (i % n) + 1
+            turn = (i // n) + 1
+            return [person, turn]
+        
+        used_words.add(words[i])
+
+    return [0, 0]
