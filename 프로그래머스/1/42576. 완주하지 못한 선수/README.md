1f079cf4be3fd1e3d7336db23796a2d7d9821101
taeyoung0823 <154206361+taeyoung0823@users.noreply.github.com>
2025-01-06 10:42:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.49 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 33.9 MB, 시간: 45.47 ms
+메모리: 38.9 MB, 시간: 66.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 06일 10:39:53
+2025년 01월 06일 10:42:24
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,6 @@
+import collections
+
+
 def solution(participant, completion):
-    part_dict = {}
-    for name in participant:
-        if name in part_dict:
-            part_dict[name] += 1
-        else:
-            part_dict[name] = 1
-
-    for name in completion:
-        if name in part_dict:
-            part_dict[name] -= 1
-
-    for key, value in part_dict.items():
-        if value == 1:
-            return key
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
