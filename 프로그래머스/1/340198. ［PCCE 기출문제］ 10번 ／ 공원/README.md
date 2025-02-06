e2ba821b4a1e7643b869d57888bd65dfb2217680
taeyoung0823 <154206361+taeyoung0823@users.noreply.github.com>
2025-02-06 11:26:07 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 7.94 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/README.md
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 10번 / 공원 - 340198 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340198#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340198) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 7.85 ms
+메모리: 10.4 MB, 시간: 7.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 06일 11:23:29
+2025년 02월 06일 11:26:06
 
 ### 문제 설명
 
--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,5 +1,4 @@
 def can_place_mat(park, size, start_row, start_col):
-    # 주어진 위치에서 size x size 크기의 돗자리를 놓을 수 있는지 확인
     for i in range(start_row, start_row + size):
         for j in range(start_col, start_col + size):
             if park[i][j] != "-1":
@@ -7,7 +6,6 @@ def can_place_mat(park, size, start_row, start_col):
     return True
 
 def solution(mats, park):
-    # 돗자리 크기를 내림차순으로 정렬
     mats.sort(reverse=True)
     rows = len(park)
     cols = len(park[0])
